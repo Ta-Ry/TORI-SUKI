cefba07f3ded323998b13ca35a3f177bbdb975e1
田中良征 <siro1.sou.roi.kira3@gmail.com>
2020-04-29 13:11:27 +0900

    [Fix]user,postモデル

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,47 +1,8 @@
 class Post < ApplicationRecord
-	before_action :authenticate_user!
-	def index
-  	@posts = Post.all
-  	@post = Post.new
-    @user = current_user
-  end
-
-  def show
-  	@post = Post.find(params[:id])
-    @user = @post.user
-    @post_comment = PostComment.new
-  end
-
-  def edit
-  	@post = Post.find(params[:id])
-    @user = @post.user
-    if current_user != @user
-      redirect_to posts_path
-    end
-  end
-
-  def create
-  	@post = Post.new(post_params)
-  	@post.user_id = current_user.id
-  	if @post.save
-  		flash[:notice] = "投稿しました！"
-  	    redirect_to post_path(@post)
-  	else
-  		@posts = Post.all
-      @user = current_user
-  		render action: :index
-  	end
-  end
-
-  def destroy
-  	@post = Post.find(params[:id])
-  	@post.destroy
-  	redirect_to posts_path
-  end
-
-  private
-
-  def post_params
-  	params.require(:post).permit(:title, :body)
-  end
+	belongs_to :user
+  has_many :favorites, dependent: :destroy
+        def favorited_by?(user)
+          favorites.where(user_id: user.id).exists?
+        end
+  has_many :post_comments, dependent: :destroy
 end
