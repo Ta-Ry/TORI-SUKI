76ff292efa3cdf3178067f47d248d4cb5641b0b0
田中良征 <siro1.sou.roi.kira3@gmail.com>
2020-04-30 22:11:52 +0900

    [Fix]JS、SCSSファイル&[Add]画像投稿用キーの追加、no_image_fileの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '081f69206701da79ad80962b205cbf1daa42c6f754fd6719e29d21ace9fced25fd1d03409478f746ec0c992e7adba92271bfb344f6593a2851b9968819d3f6f6'
