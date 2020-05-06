7b58ee5ac1cf2f90759ed9ff5aa44e664886451c
田中良征 <siro1.sou.roi.kira3@gmail.com>
2020-05-07 01:04:00 +0900

    [Add]バリデート

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
 	belongs_to :user
-  has_many :favorites, dependent: :destroy
+  	has_many :favorites, dependent: :destroy
         def favorited_by?(user)
           favorites.where(user_id: user.id).exists?
         end
-  has_many :post_comments, dependent: :destroy
-  attachment :post_image
-  acts_as_taggable
+  	has_many :post_comments, dependent: :destroy
+  	attachment :post_image
+  	acts_as_taggable
+	validates :main_comment, presence: true, length: {maximum: 200}
+	validates :tag_list, presence: true
 end
