90c981d2326eb31ef35beb7121e53133864f73e1
田中良征 <siro1.sou.roi.kira3@gmail.com>
2020-05-09 23:37:38 +0900

    [Fix]view調整、画像追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,21 @@
 class HomeController < ApplicationController
-	def top
-	end
+  def top
+  end
 
-	def about
-	end
+  def about
+  end
 
-	def bird_hospital
-	end
+  def bird_hospital
+  end
 
-	def lost_bird
-	end
+  def lost_bird
+	@posts = Post.all
+    @tag = Tag.where(tag_name = "迷い鳥情報")
+    if params[:tag_name]
+    	@posts = Post.tagged_with("#{params[:tag_name]}")
+    end
+  end
 
-	def contact
-	end
+  def contact
+  end
 end
