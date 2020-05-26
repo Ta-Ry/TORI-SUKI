079b9f01b0030085b29fe2221dfd238a405cbb7e
田中良征 <siro1.sou.roi.kira3@gmail.com>
2020-05-26 15:37:56 +0900

    [Add]画像認識機能追加に伴う変更と追加

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module TORISuki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = :ja #5/25追加
+    config.paths.add 'lib', eager_load: true # 5/15追加
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
